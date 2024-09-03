fc1096019d6f879e75a79d8fbd11394afbd4b339
Aman Shaikh <amanshaikh8624@gmail.com>
2024-09-03 16:09:42 +0530

    service grid added

--- frontend/src/components/Services.js
+++ frontend/src/components/Services.js
@@ -5,21 +5,172 @@ const Services = () => {
     <div>
       <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:mx-20 gap-4 p-4">
         {/* Card 1 */}
-        <div className="border rounded p-4 flex flex-col items-center max-w-max justify-center">
+        <div className="border rounded p-4 flex flex-col items-center w-full max-w-xs justify-center mx-auto">
           <div className="flex items-center justify-center h-28">
-            <img className='h-full object-cover' src='./Diabetes.webp' alt="Diabetes"/>
+            <img className="h-full object-cover" src="./Diabetes.webp" alt="Diabetes" />
           </div>
           <div className="mt-4 text-center">
-            <h1 className='font-semibold break-words'>Complete Blood Count</h1>
+            <h1 className="font-semibold break-words">Complete Blood Count</h1>
           </div>
-          <div className="text-sm text-center">
-            <h1 className=' break-words'>Complete Blood Count</h1>
+
+          <div className="text-sm text-center mt-2 text-gray-600 w-fit">
+            <p className="line-clamp-1 break-words">A comprehensive blood test that measures various components and features of your blood.</p>
           </div>
-          <div className='flex items-center justify-between w-full mt-8'>
-            <h1 className='font-bold text-xl'>₹399</h1>
-            <button className='font-bold text-xl text-red-500'>Add</button>
+          <div className="flex items-center justify-center mt-2">
+            <span className="text-yellow-500">★★★★☆</span>
+            <span className="text-gray-600 text-sm ml-2">(120 reviews)</span>
+          </div>
+          <div className="flex items-center justify-between w-full mt-8">
+            <h1 className="font-bold text-xl">₹399</h1>
+            <button className="font-bold text-xl text-red-500">Add</button>
           </div>
         </div>
+
+
+
+
+
+        <div className="border rounded p-4 flex flex-col items-center w-full max-w-xs justify-center mx-auto">
+          <div className="flex items-center justify-center h-28">
+            <img className="h-full object-cover" src="./Diabetes.webp" alt="Diabetes" />
+          </div>
+          <div className="mt-4 text-center">
+            <h1 className="font-semibold break-words">Complete Blood Count</h1>
+          </div>
+
+          <div className="text-sm text-center mt-2 text-gray-600 w-fit">
+            <p className="line-clamp-1 break-words">A comprehensive blood test that measures various components and features of your blood.</p>
+          </div>
+          <div className="flex items-center justify-center mt-2">
+            <span className="text-yellow-500">★★★★☆</span>
+            <span className="text-gray-600 text-sm ml-2">(120 reviews)</span>
+          </div>
+          <div className="flex items-center justify-between w-full mt-8">
+            <h1 className="font-bold text-xl">₹399</h1>
+            <button className="font-bold text-xl text-red-500">Add</button>
+          </div>
+        </div>
+
+
+
+
+        <div className="border rounded p-4 flex flex-col items-center w-full max-w-xs justify-center mx-auto">
+          <div className="flex items-center justify-center h-28">
+            <img className="h-full object-cover" src="./Diabetes.webp" alt="Diabetes" />
+          </div>
+          <div className="mt-4 text-center">
+            <h1 className="font-semibold break-words">Complete Blood Count</h1>
+          </div>
+
+          <div className="text-sm text-center mt-2 text-gray-600 w-fit">
+            <p className="line-clamp-1 break-words">A comprehensive blood test that measures various components and features of your blood.</p>
+          </div>
+          <div className="flex items-center justify-center mt-2">
+            <span className="text-yellow-500">★★★★☆</span>
+            <span className="text-gray-600 text-sm ml-2">(120 reviews)</span>
+          </div>
+          <div className="flex items-center justify-between w-full mt-8">
+            <h1 className="font-bold text-xl">₹399</h1>
+            <button className="font-bold text-xl text-red-500">Add</button>
+          </div>
+        </div>
+
+
+
+
+        <div className="border rounded p-4 flex flex-col items-center w-full max-w-xs justify-center mx-auto">
+          <div className="flex items-center justify-center h-28">
+            <img className="h-full object-cover" src="./Diabetes.webp" alt="Diabetes" />
+          </div>
+          <div className="mt-4 text-center">
+            <h1 className="font-semibold break-words">Complete Blood Count</h1>
+          </div>
+
+          <div className="text-sm text-center mt-2 text-gray-600 w-fit">
+            <p className="line-clamp-1 break-words">A comprehensive blood test that measures various components and features of your blood.</p>
+          </div>
+          <div className="flex items-center justify-center mt-2">
+            <span className="text-yellow-500">★★★★☆</span>
+            <span className="text-gray-600 text-sm ml-2">(120 reviews)</span>
+          </div>
+          <div className="flex items-center justify-between w-full mt-8">
+            <h1 className="font-bold text-xl">₹399</h1>
+            <button className="font-bold text-xl text-red-500">Add</button>
+          </div>
+        </div>
+
+
+
+
+        <div className="border rounded p-4 flex flex-col items-center w-full max-w-xs justify-center mx-auto">
+          <div className="flex items-center justify-center h-28">
+            <img className="h-full object-cover" src="./Diabetes.webp" alt="Diabetes" />
+          </div>
+          <div className="mt-4 text-center">
+            <h1 className="font-semibold break-words">Complete Blood Count</h1>
+          </div>
+
+          <div className="text-sm text-center mt-2 text-gray-600 w-fit">
+            <p className="line-clamp-1 break-words">A comprehensive blood test that measures various components and features of your blood.</p>
+          </div>
+          <div className="flex items-center justify-center mt-2">
+            <span className="text-yellow-500">★★★★☆</span>
+            <span className="text-gray-600 text-sm ml-2">(120 reviews)</span>
+          </div>
+          <div className="flex items-center justify-between w-full mt-8">
+            <h1 className="font-bold text-xl">₹399</h1>
+            <button className="font-bold text-xl text-red-500">Add</button>
+          </div>
+        </div>
+
+
+
+
+        <div className="border rounded p-4 flex flex-col items-center w-full max-w-xs justify-center mx-auto">
+          <div className="flex items-center justify-center h-28">
+            <img className="h-full object-cover" src="./Diabetes.webp" alt="Diabetes" />
+          </div>
+          <div className="mt-4 text-center">
+            <h1 className="font-semibold break-words">Complete Blood Count</h1>
+          </div>
+
+          <div className="text-sm text-center mt-2 text-gray-600 w-fit">
+            <p className="line-clamp-1 break-words">A comprehensive blood test that measures various components and features of your blood.</p>
+          </div>
+          <div className="flex items-center justify-center mt-2">
+            <span className="text-yellow-500">★★★★☆</span>
+            <span className="text-gray-600 text-sm ml-2">(120 reviews)</span>
+          </div>
+          <div className="flex items-center justify-between w-full mt-8">
+            <h1 className="font-bold text-xl">₹399</h1>
+            <button className="font-bold text-xl text-red-500">Add</button>
+          </div>
+        </div>
+
+
+
+
+        <div className="border rounded p-4 flex flex-col items-center w-full max-w-xs justify-center mx-auto">
+          <div className="flex items-center justify-center h-28">
+            <img className="h-full object-cover" src="./Diabetes.webp" alt="Diabetes" />
+          </div>
+          <div className="mt-4 text-center">
+            <h1 className="font-semibold break-words">Complete Blood Count</h1>
+          </div>
+
+          <div className="text-sm text-center mt-2 text-gray-600 w-fit">
+            <p className="line-clamp-1 break-words">A comprehensive blood test that measures various components and features of your blood.</p>
+          </div>
+          <div className="flex items-center justify-center mt-2">
+            <span className="text-yellow-500">★★★★☆</span>
+            <span className="text-gray-600 text-sm ml-2">(120 reviews)</span>
+          </div>
+          <div className="flex items-center justify-between w-full mt-8">
+            <h1 className="font-bold text-xl">₹399</h1>
+            <button className="font-bold text-xl text-red-500">Add</button>
+          </div>
+        </div>
+
 
       </div>
     </div>
