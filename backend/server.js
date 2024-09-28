43a19ea0f05155a4550a625ba4931490e6095c9d
Aman Shaikh <amanshaikh8624@gmail.com>
2024-09-28 15:22:58 +0530

    made changes for deployement

--- backend/server.js
+++ backend/server.js
@@ -5,7 +5,7 @@ const connectDB = require('./config/db.js');
 const authRoutes = require('./routes/authRoutes.js');
 require('dotenv').config();
 const cors = require('cors')
-
+const path = require('path');
 const app = express();
 app.use(bodyParser.json());
 app.use(cookieParser()); // Add cookie-parser middleware
@@ -15,6 +15,14 @@ connectDB();
 
 // Use the auth routes
 app.use('/api/auth', authRoutes);
+
+// Serve static files from the React app
+app.use(express.static(path.join(__dirname, '../frontend/build')));
+
+// The catchall handler for any request that doesn't match one above
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, '../frontend/build', 'index.html'));
+});
 
 // Start the server
 const PORT = process.env.PORT || 5000;
